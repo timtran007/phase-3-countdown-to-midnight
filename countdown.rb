7f194f8d44c878e0b87cfcdd4fa0b8cc7d3584ea
Tim <timtran007@gmail.com>
2022-12-28 21:05:40 -0600

    commit tree 3f86066ec72c58f43126c8052ce720bec3cb5788

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+require 'pry'
+def countdown (n)
+    while n >= 1 
+        puts "#{n} SECOND(S)!"
+        n -=1
+    end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep (n)
+    while n >= 1 
+        sleep(1)
+        puts "#{n} SECOND(S)!"
+        n -=1
+    end
+    "HAPPY NEW YEAR!"
 end
